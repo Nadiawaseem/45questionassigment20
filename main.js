be06b97e5dff938c009849cf4ba0f6b2e57c8e29
nadiawaseem <161966596+Nadiawaseem@users.noreply.github.com>
2024-06-08 16:58:31 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+let array = ["english", "urdu", "sindhi", "punjabi", "saraiki"];
+array.forEach(array => {
+    console.log(array);
+});
---
+++ main.ts
@@ -0,0 +1,7 @@
+  let array =["english","urdu","sindhi","punjabi","saraiki"];
+
+  
+ array.forEach(array=> {console.log(array);
+
+ });
+
